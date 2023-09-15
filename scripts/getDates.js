fbf7c9e94ecdb44200de3ae2d804d814b27e13ee
CJLynch01 <cjlynch@byui.edu>
2023-09-14 22:42:20 -0600

    Save

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,5 @@
 var CurrentYear = new Date().getFullYear();
 document.querySelector('#year').innerHTML = CurrentYear
+
+const date = new Date(document.lastModified);
+document.querySelector('#date').innerHTML = date
