77a9f0aee6d52f6615d6f267b20832d3666ed323
CJLynch01 <cjlynch@byui.edu>
2023-09-16 21:55:06 -0600

    Save

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,7 @@
+//Get's the current year for the copywrite date.
 var CurrentYear = new Date().getFullYear();
 document.querySelector('#year').innerHTML = CurrentYear
 
+//Updates the last modification date.
 const date = new Date(document.lastModified);
 document.querySelector('#date').innerHTML = date
