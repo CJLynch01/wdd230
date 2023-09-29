e4628069bca4d2c8d664daf4423c130fffe9bd07
CJLynch01 <cjlynch@byui.edu>
2023-09-28 21:10:08 -0600

    Hamburger Menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,12 @@ document.querySelector('#year').innerHTML = CurrentYear
 //Updates the last modification date.
 const date = new Date(document.lastModified);
 document.querySelector('#date').innerHTML = date
+
+//Button for hamburger menu.
+const hamButton = document.querySelector('#menu');
+const navi = document.querySelector('.navi');
+
+hamButton.addEventListener('click', () => {
+	navi.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
