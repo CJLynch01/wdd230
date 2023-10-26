d8e31545761fc3e9e77bc0123552d66f568602a2
CJLynch01 <cjlynch@byui.edu>
2023-10-25 21:58:27 -0600

    Change to Visits

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -18,9 +18,9 @@ hamButton.addEventListener('click', () => {
 //visits
 const visitsDisplay = document.querySelector(".visits");
 
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 1;
 
-if (numVisits !== 0) {
+if (numVisits !== 1) {
 	visitsDisplay.textContent = numVisits;
 } else {
 	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
