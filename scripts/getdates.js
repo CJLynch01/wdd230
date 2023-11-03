7280bf1eeb21d62292722d69eb3df1fb426c5c1a
CJLynch01 <cjlynch@byui.edu>
2023-11-02 22:12:01 -0600

    Form for Home Page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -29,3 +29,38 @@ if (numVisits !== 1) {
 numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
+
+
+//input range for form
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+//Checks password value to be the same
+
+const kp1 = document.querySelector("#pwd");
+const kp2 = document.querySelector("#pwd2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
