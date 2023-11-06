5e904b79830685c5d538fdb580d98a4e96c63e0f
CJLynch01 <cjlynch@byui.edu>
2023-11-06 16:44:49 -0700

    JSON HTML page for presentation

---
+++ json/scripts/json.js
@@ -0,0 +1,22 @@
+fetch("cities.json")
+
+.then(function(response) {
+    return response.json();
+})
+
+.then(function(cities) {
+    let placeholder = document.querySelector("#data-output");
+    let out = "";
+    for(let city of cities){
+        out += `
+            <tr>
+                <td>${city.name}</td>
+                <td>${city.state_name}</td>
+                <td>${city.country_name}</td>
+            </tr>
+        
+        `;
+    }
+
+    placeholder.innerHTML = out;
+})
