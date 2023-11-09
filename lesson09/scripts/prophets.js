2ddc909d462802ae9015a83a525422c3f5bd2b4a
CJLynch01 <cjlynch@byui.edu>
2023-11-08 20:32:16 -0700

    Save

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -12,21 +12,21 @@ getProphetData();
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-      let card = document.createElement('section');
-      let fullName = document.createElement('h2');
-      let portrait = document.createElement('img');
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
   
-      fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
 
-      portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.fullName}`);
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
   
-      card.appendChild(fullName);
-      card.appendChild(portrait);
+        card.appendChild(fullName);
+        card.appendChild(portrait);
   
-      cards.appendChild(card);
+        cards.appendChild(card);
     });
   }
