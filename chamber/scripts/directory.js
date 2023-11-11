d689da9b5c755a5800a3d9f60c5bc72f540d2cb5
CJLynch01 <cjlynch@byui.edu>
2023-11-10 22:25:27 -0700

    Save

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,48 @@
+const url = "https://cjlynch01.github.io/wdd230/chamber/data/members.json";
+const cards = document.querySelector('#cards');
+
+async function getBusinessData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    displaybusinesses(data.businesses);
+}
+
+getBusinessData();
+
+const displaybusinesses = (businesses) => {
+
+    businesses.forEach((business) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let address = document.createElement('li');
+        let phone = document.createElement('li');
+        let website = document.createElement('li');
+        let membership = document.createElement('li');
+        let information = document.createElement('li');
+    
+
+        portrait.setAttribute('src', business.image);
+        portrait.setAttribute('alt', `Picture of ${business.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '250');
+        portrait.setAttribute('height', '200');
+
+
+
+
+
+        card.appendChild(name);
+        card.appendChild(portrait);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(membership);
+        card.appendChild(information);
+
+        cards.appendChild(card);
+
+    });
+
+}
--- scripts/links.js
+++ scripts/links.js
@@ -5,17 +5,17 @@ const cards = document.querySelector('#cards');
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data)
+    //console.log(data)
     displayLinks(data.lessons);
 }
   
 getLinks();
 
 const displayLinks = (lessons) => {
-    console.log(lessons)
+    //console.log(lessons)
     lessons.forEach((week) => {
         let card = document.createElement('section');
-        console.log(week)
+        //console.log(week)
         let numbers = document.createElement('li');
         let description = document.createElement('title');
 
