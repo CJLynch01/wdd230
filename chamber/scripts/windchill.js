358474674d72b03ce1181428989abf5c7e998ce2
CJLynch01 <cjlynch@byui.edu>
2023-11-18 18:37:51 -0700

    Save

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,3 +4,41 @@ var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.42
 
 var windChill= Math.round(windChill);
 document.getElementById("windChill").innerHTML= windChill;
+
+
+//temperature
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=41.17&lon=-111.98&appid=d8a9e71401b6b1b5681eae6b4172a2d7&units=imperial';
+
+async function apiFetch() {
+	console.log("test")
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+                //console.log(data)
+                displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }   catch (error) {
+            console.log(error);
+    }
+    
+}
+
+function displayResults(data) {
+	console.log(data)
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc );
+    weatherIcon.setAttribute('alt', `Weather is ${data.weather.description}`)
+
+	captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
